41ca500f7b6625ecc80b6ac1d664f1969060c00e
Chase Taylor <x8@live.ca>
2023-10-04 11:22:19 -0600

    oof

--- deps/7th Heaven.sh
+++ deps/7th Heaven.sh
@@ -2,4 +2,4 @@
 shopt -s expand_aliases
 alias protontricks='flatpak run com.github.Matoking.protontricks'
 alias protontricks-launch='flatpak run --command=protontricks-launch com.github.Matoking.protontricks'
-protontricks-launch --no-bwrap --appid 39140 "/home/deck/7th Heaven/7th Heaven.exe"
+protontricks-launch --no-bwrap --appid 39140 "7th Heaven.exe"
