620a2d83a953344d0e0d3985f6dd9974055a76c2
Chase Taylor <x8@live.ca>
2023-10-05 16:18:09 -0600

    Exit if proton or soldier aren't found

--- deps/7th Heaven.sh
+++ deps/7th Heaven.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-PROTON_DIR=$(if [ -d "${HOME}/.local/share/Steam/steamapps/common/Proton 8.0" ]; then echo "${HOME}/.local/share/Steam/steamapps/common/Proton 8.0"; else echo "/run/media/mmcblk0p1/steamapps/common/Proton 8.0"; fi)
-SOLDIER_DIR=$(if [ -d "${HOME}/.local/share/Steam/steamapps/common/SteamLinuxRuntime_soldier" ]; then echo "${HOME}/.local/share/Steam/steamapps/common/SteamLinuxRuntime_soldier"; else echo "/run/media/mmcblk0p1/steamapps/common/SteamLinuxRuntime_soldier"; fi)
-[ ! -d "$PROTON_DIR" ] && echo "Proton not found!"
-[ ! -d "$SOLDIER_DIR" ] && echo "SteamLinuxRuntime Soldier not found!"
 export LD_LIBRARY_PATH="$PROTON_DIR/dist/lib:$PROTON_DIR/dist/lib64:$LD_LIBRARY_PATH"
 export STEAM_COMPAT_DATA_PATH="${HOME}/.local/share/Steam/steamapps/compatdata/39140"
 export STEAM_COMPAT_CLIENT_INSTALL_PATH="${HOME}/.local/share/Steam"
 export STEAM_RUNTIME=0
+
+PROTON_HOME="${HOME}/.local/share/Steam/steamapps/common/Proton 8.0"
+PROTON_SD="/run/media/mmcblk0p1/steamapps/common/Proton 8.0"
+SOLDIER_HOME="${HOME}/.local/share/Steam/steamapps/common/SteamLinuxRuntime_soldier"
+SOLDIER_SD="/run/media/mmcblk0p1/steamapps/common/SteamLinuxRuntime_soldier"
+
+[ -d "$PROTON_HOME" ] && PROTON_DIR="$PROTON_HOME" || \
+{ [ -d "$PROTON_SD" ] && PROTON_DIR="$PROTON_SD" || \
+{ echo "Proton not found!"; exit 1; }; }
+
+[ -d "$SOLDIER_HOME" ] && SOLDIER_DIR="$SOLDIER_HOME" || \
+{ [ -d "$SOLDIER_SD" ] && SOLDIER_DIR="$SOLDIER_SD" || \
+{ echo "Soldier not found!"; exit 1; }; }
+
 ${HOME}/.local/share/Steam/ubuntu12_32/reaper --filesystem /home/ SteamLaunch AppId=39140 -- "$SOLDIER_DIR/_v2-entry-point" --verb=waitforexitandrun -- "$PROTON_DIR/proton" waitforexitandrun "7th Heaven.exe"
