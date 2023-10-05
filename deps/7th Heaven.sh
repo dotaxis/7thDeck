d2d8241b7e4029797b76f9e47751a99fd4d5b952
Chase Taylor <x8@live.ca>
2023-10-04 23:43:56 -0600

    Fixed Steam Compatibility in Desktop mode, still need fix for Game Mode

--- deps/7th Heaven.sh
+++ deps/7th Heaven.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-shopt -s expand_aliases
-alias protontricks='flatpak run com.github.Matoking.protontricks'
-alias protontricks-launch='flatpak run --command=protontricks-launch com.github.Matoking.protontricks'
-protontricks-launch --no-bwrap --appid 39140 "7th Heaven.exe"
+export LD_LIBRARY_PATH=="${HOME}/.local/share/Steam/steamapps/common/Proton 8.0/dist/lib:${HOME}/.local/share/Steam/steamapps/common/Proton 8.0/dist/lib64:$LD_LIBRARY_PATH"
+export STEAM_COMPAT_DATA_PATH="${HOME}/.local/share/Steam/steamapps/compatdata/39140"
+export STEAM_COMPAT_CLIENT_INSTALL_PATH="${HOME}/.local/share/Steam"
+export STEAM_RUNTIME=0
+/home/deck/.local/share/Steam/ubuntu12_32/reaper --filesystem /home/ SteamLaunch AppId=39140 -- '/home/deck/.local/share/Steam/steamapps/common/SteamLinuxRuntime_soldier/_v2-entry-point' --verb=waitforexitandrun -- '/home/deck/.local/share/Steam/steamapps/common/Proton 8.0/proton' waitforexitandrun "7th Heaven.exe"
