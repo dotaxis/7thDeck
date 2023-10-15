dc911fc024bd1a6e0724680639a674ecce4662d7
Chase Taylor <x8@live.ca>
2023-10-15 06:01:04 -0600

    Stop using Steam Reaper (we don't need it)

--- deps/7th Heaven.sh
+++ deps/7th Heaven.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
+export STEAM_COMPAT_APP_ID=39140
 export STEAM_COMPAT_DATA_PATH="WINEPATH"
-export STEAM_COMPAT_CLIENT_INSTALL_PATH="${HOME}/.local/share/Steam"
-export STEAM_RUNTIME=0
+export STEAM_COMPAT_CLIENT_INSTALL_PATH=$(readlink -f "$HOME/.steam/root")
 
-REAPER="${HOME}/.local/share/Steam/ubuntu12_32/reaper"
 SNIPER_HOME="${HOME}/.local/share/Steam/steamapps/common/SteamLinuxRuntime_sniper/_v2-entry-point"
 SNIPER_SD="/run/media/mmcblk0p1/steamapps/common/SteamLinuxRuntime_sniper/_v2-entry-point"
 PROTON_HOME="${HOME}/.local/share/Steam/steamapps/common/Proton 8.0/proton"
 PROTON_SD="/run/media/mmcblk0p1/steamapps/common/Proton 8.0/proton"
-
-[ ! -f "$REAPER" ] && { kdialog --error  "Reaper not found!"; exit 1; }
 
 [ -f "$SNIPER_HOME" ] && SNIPER="$SNIPER_HOME" || \
 { [ -f "$SNIPER_SD" ] && SNIPER="$SNIPER_SD" || \
@@ -19,5 +16,4 @@ PROTON_SD="/run/media/mmcblk0p1/steamapps/common/Proton 8.0/proton"
 { [ -f "$PROTON_SD" ] && PROTON="$PROTON_SD" || \
 { kdialog --error  "Proton not found!"; exit 1; }; }
 
-"$REAPER" SteamLaunch AppId=39140 -- "$SNIPER" -- "$PROTON" waitforexitandrun \
-"7th Heaven.exe" $*
+"$SNIPER" -- "$PROTON" waitforexitandrun "7th Heaven.exe" $*
