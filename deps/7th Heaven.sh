0a6423a52d792982855825be6224f38f1cb91b63
Chase Taylor <x8@live.ca>
2024-01-20 10:00:33 -0700

    Hardcode runtime and proton paths in launcher for now

--- deps/7th Heaven.sh
+++ deps/7th Heaven.sh
@@ -3,8 +3,8 @@ export STEAM_COMPAT_APP_ID=39140
 export STEAM_COMPAT_DATA_PATH="WINEPATH"
 export STEAM_COMPAT_CLIENT_INSTALL_PATH=$(readlink -f "$HOME/.steam/root")
 
-RUNTIME="${HOME}/.local/share/Steam/steamapps/common/SteamLinuxRuntime_soldier/run"
-PROTON="${HOME}/.local/share/Steam/steamapps/common/Proton 7.0/proton"
+RUNTIME="RUNTIME_PATH"
+PROTON="PROTON_PATH"
 
 [ ! -f "$RUNTIME" ] && { kdialog --error  "SteamLinuxRuntime not found!"; exit 1; }
 
