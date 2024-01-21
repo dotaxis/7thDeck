7369ff4e8297dcf6cc4ce6bd9ffed3d6f05e6961
Sander Sweers <infirit@gmail.com>
2024-01-21 14:25:49 +0100

    Use @ as pre/suffix when updating 7th Heaven.sh

--- deps/7th Heaven.sh
+++ deps/7th Heaven.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 export STEAM_COMPAT_APP_ID=39140
-export STEAM_COMPAT_DATA_PATH="WINEPATH"
+export STEAM_COMPAT_DATA_PATH="@WINEPATH@"
 export STEAM_COMPAT_CLIENT_INSTALL_PATH=$(readlink -f "$HOME/.steam/root")
-export STEAM_COMPAT_MOUNTS="MOUNTS"
+export STEAM_COMPAT_MOUNTS="@MOUNTS@"
 
-RUNTIME="RUNTIME_PATH"
-PROTON="PROTON_PATH"
+RUNTIME="@RUNTIME_PATH@"
+PROTON="@PROTON_PATH@"
 
 [ ! -f "$RUNTIME" ] && { kdialog --error  "SteamLinuxRuntime not found!"; exit 1; }
 
