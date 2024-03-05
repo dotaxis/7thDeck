30dec57c677a1ba08ec5e8f6d6f8253c6eac52bb
Chase Taylor <11805686+dotaxis@users.noreply.github.com>
2024-03-05 11:48:57 -0700

    Rework launcher to account for change in environment after installation

--- deps/7th Heaven.sh
+++ deps/7th Heaven.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-unset DOTNET_ROOT
-export PATH=$(echo "${PATH}" | sed -e "s|:$HOME/dotnet||")
+. functions.sh
+
+IS_STEAMOS=@STEAMOS@
+if [ $IS_STEAMOS = true ] ; then
+  export STEAM_COMPAT_DATA_PATH=${HOME}/.steam/steam/steamapps/compatdata/39140
+else
+  export STEAM_COMPAT_DATA_PATH=$(LIBRARY=$(getSteamLibrary 39140) && [ -n "$LIBRARY" ] && echo "$LIBRARY/steamapps/compatdata/39140" || echo "NONE")
+fi
+
 export STEAM_COMPAT_APP_ID=39140
-export STEAM_COMPAT_DATA_PATH="@WINEPATH@"
 export STEAM_COMPAT_CLIENT_INSTALL_PATH=$(readlink -f "$HOME/.steam/root")
-export STEAM_COMPAT_MOUNTS="@MOUNTS@"
+export STEAM_COMPAT_MOUNTS="$(getSteamLibrary 1493710):$(getSteamLibrary 1628350):${STEAM_COMPAT_DATA_PATH%/steamapps/compatdata/39140}"
 export WINEDLLOVERRIDES="dinput=n,b"
+export PATH=$(echo "${PATH}" | sed -e "s|:$HOME/dotnet||")
+unset DOTNET_ROOT
 
+RUNTIME=$(LIBRARY=$(getSteamLibrary 1628350) && [ -n "$LIBRARY" ] && echo "$LIBRARY/steamapps/common/SteamLinuxRuntime_sniper/run" || echo "NONE")
+PROTON=$(LIBRARY=$(getSteamLibrary 1493710) && [ -n "$LIBRARY" ] && echo "$LIBRARY/steamapps/common/Proton - Experimental/proton" || echo "NONE")
 
-RUNTIME="@RUNTIME_PATH@"
-PROTON="@PROTON_PATH@"
+[ ! -d "$STEAM_COMPAT_DATA_PATH" ] && { promptUser  "FF7 prefix not found!"; exit 1; }
+[ "$RUNTIME" = "NONE" ] && { promptUser  "Steam Linux Runtime not found!"; exit 1; }
+[ "$PROTON" = "NONE" ] && { promptUser  "Proton not found!"; exit 1; }
 
-[ ! -f "$RUNTIME" ] && { kdialog --error  "SteamLinuxRuntime not found!"; exit 1; }
-
-[ ! -f "$PROTON" ] && { kdialog --error  "Proton 7.0 not found!"; exit 1; }
 
 "$RUNTIME" -- "$PROTON" waitforexitandrun "$PWD/7th Heaven.exe" $*
