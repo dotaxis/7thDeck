bf046c0ad408e65738776a6946232a7bce618f16
Chase Taylor <11805686+dotaxis@users.noreply.github.com>
2024-04-19 12:24:24 -0600

    Update 2.3 (#35)
    * Force Proton Experimental and use SLR3
    
    * Rework checking for Proton/SLR/FF7
    
    * Use XDG_DATA_HOME for update-desktop-database
    
    * Rework launcher to account for change in environment after installation
    
    * Add logging for 7th installer and hide popups
    
    * Account for spaces in $SEVENTH_INSTALLER path
    
    * Update .gitignore to account for 7thHeaven.log
    
    * Suppress General Settings window on first launch
    
    * Bump version to 2.3

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 . deps/functions.sh
-export STEAM_COMPAT_MOUNTS="$(getSteamLibrary 1887720):$(getSteamLibrary 1391110):$(getSteamLibrary 39140)"
-PROTON=$(LIBRARY=$(getSteamLibrary 1887720) && [ -n "$LIBRARY" ] && echo "$LIBRARY/steamapps/common/Proton 7.0/proton" || echo "NONE")
-RUNTIME=$(LIBRARY=$(getSteamLibrary 1391110) && [ -n "$LIBRARY" ] && echo "$LIBRARY/steamapps/common/SteamLinuxRuntime_soldier/run" || echo "NONE")
-FF7_LIBRARY=$(getSteamLibrary 39140 || echo "NONE")
 XDG_DESKTOP_DIR=$(xdg-user-dir DESKTOP)
 XDG_DATA_HOME="${XDG_DATA_HOME:=${HOME}/.local/share}"
 IS_STEAMOS=$(grep -qi "SteamOS" /etc/os-release && echo true || echo false)
@@ -12,7 +8,7 @@ echo "" > "7thDeck.log"
 exec > >(tee -ia "7thDeck.log") 2>&1
 
 echo "########################################################################"
-echo "#                             7thDeck v2.1                             #"
+echo "#                             7thDeck v2.3                             #"
 echo "########################################################################"
 echo "#    This script will:                                                 #"
 echo "#   1. Apply patches to FF7's proton prefix to accomodate 7th Heaven   #"
@@ -26,51 +22,86 @@ echo "#   or ask in the #Steamdeck-Proton channel of the Tsunamods Discord   #"
 echo "########################################################################"
 echo -e "\n"
 
+# Check for Proton
+while true; do
+  if ! pgrep steam > /dev/null; then nohup steam &> /dev/null; fi
+  while ! pgrep steam > /dev/null; do sleep 1; done
+  PROTON=$(LIBRARY=$(getSteamLibrary 1493710) && [ -n "$LIBRARY" ] && echo "$LIBRARY/steamapps/common/Proton - Experimental/proton" || echo "NONE")
+  echo -n "Checking if Proton Experimental is installed... "
+  if [ "$PROTON" = "NONE" ]; then
+    echo -e "\nNot found! Launching Steam to install."
+    nohup steam steam://install/1493710 &> /dev/null &
+    read -p "Press Enter when Proton Experimental is done installing."
+    pkill -9 steam
+    while pgrep steam >/dev/null; do sleep 1; done
+    rm $HOME/.steam/steam/steamapps/libraryfolders.vdf &>> "7thDeck.log"
+    rm $HOME/.steam/steam/config/libraryfolders.vdf &>> "7thDeck.log"
+  else
+    echo "OK!"
+    echo "Found Proton at $PROTON!"
+    echo
+    break
+  fi
+done
 
-# Check for Proton 7
-echo -n "Checking if Proton 7 is installed... "
-if [ "$PROTON" = "NONE" ]; then
-  echo -e "\nNot found! Launching Steam to install."
-  nohup steam steam://install/1887720 &> /dev/null &
-  echo "Re-run this script when Proton 7 is done installing."
-  read -p "Press Enter to close this window."
-  exit 1
-fi
-echo "OK!"
 # Check for SteamLinuxRuntime
-echo -n "Checking if SteamLinuxRuntime 2.0 is installed... "
-if [ "$RUNTIME" = "NONE" ]; then
-  echo -e "\nNot found! Launching Steam to install."
-  nohup steam steam://install/1391110 &> /dev/null &
-  echo "Re-run this script when SteamLinuxRuntime 2.0 (Soldier) is done installing."
-  read -p "Press Enter to close this window."
-  exit 1
-fi
-echo "OK!"
-# Check for FF7 and set paths
-echo -n "Checking if FF7 is installed... "
-if [ "$FF7_LIBRARY" = "NONE" ]; then
-  echo -e "\nNot found! Launching Steam to install."
-  nohup steam steam://install/39140 &> /dev/null &
-  echo "Re-run this script when FINAL FANTASY VII is done installing."
-  read -p "Press Enter to close this window."
-  exit 1
-else
-  FF7_DIR="$FF7_LIBRARY/steamapps/common/FINAL FANTASY VII"
-  WINEPATH="$FF7_LIBRARY/steamapps/compatdata/39140/pfx"
-  [ $IS_STEAMOS = true ] && WINEPATH="${HOME}/.steam/steam/steamapps/compatdata/39140/pfx"
-fi
-echo "OK!"
-echo
+while true; do
+  if ! pgrep steam > /dev/null; then nohup steam &> /dev/null; fi
+  while ! pgrep steam > /dev/null; do sleep 1; done
+  RUNTIME=$(LIBRARY=$(getSteamLibrary 1628350) && [ -n "$LIBRARY" ] && echo "$LIBRARY/steamapps/common/SteamLinuxRuntime_sniper/run" || echo "NONE")
+  echo -n "Checking if Steam Linux Runtime is installed... "
+  if [ "$RUNTIME" = "NONE" ]; then
+    echo -e "\nNot found! Launching Steam to install."
+    nohup steam steam://install/1628350 &> /dev/null &
+    read -p "Press Enter when Steam Linux Runtime 3.0 (sniper) is done installing."
+    pkill -9 steam
+    while pgrep steam >/dev/null; do sleep 1; done
+    rm $HOME/.steam/steam/steamapps/libraryfolders.vdf &>> "7thDeck.log"
+    rm $HOME/.steam/steam/config/libraryfolders.vdf &>> "7thDeck.log"
+  else
+    echo "OK!"
+    echo "Found SLR at $RUNTIME!"
+    echo
+    break
+  fi
+done
 
-# Force FF7 under Proton 7
-echo "Rebuilding Final Fantasy VII under Proton 7.0..."
+# Check for FF7 and set paths
+while true; do
+  if ! pgrep steam > /dev/null; then nohup steam &> /dev/null; fi
+  while ! pgrep steam > /dev/null; do sleep 1; done
+  echo -n "Checking if FF7 is installed... "
+  FF7_LIBRARY=$(getSteamLibrary 39140 || echo "NONE")
+  if [ "$FF7_LIBRARY" = "NONE" ]; then
+    echo -e "\nNot found! Launching Steam to install."
+    nohup steam steam://install/39140 &> /dev/null &
+    read -p "Press Enter when FINAL FANTASY VII is done installing."
+    pkill -9 steam
+    while pgrep steam > /dev/null; do sleep 1; done
+    rm $HOME/.steam/steam/steamapps/libraryfolders.vdf &>> "7thDeck.log"
+    rm $HOME/.steam/steam/config/libraryfolders.vdf &>> "7thDeck.log"
+  else
+    echo "OK!"
+    echo "Found FF7 at $FF7_LIBRARY!"
+    echo
+    break
+  fi
+done
+
+# Set paths and compat_mounts after libraries have been properly detected
+FF7_DIR="$FF7_LIBRARY/steamapps/common/FINAL FANTASY VII"
+WINEPATH="$FF7_LIBRARY/steamapps/compatdata/39140/pfx"
+[ $IS_STEAMOS = true ] && WINEPATH="${HOME}/.steam/steam/steamapps/compatdata/39140/pfx"
+export STEAM_COMPAT_MOUNTS="$(getSteamLibrary 1493710):$(getSteamLibrary 1628350):$(getSteamLibrary 39140)"
+
+# Force FF7 under Proton Experimental
+echo "Rebuilding Final Fantasy VII under Proton Experimental..."
 pkill -9 steam
 cp ${XDG_DATA_HOME}/Steam/config/config.vdf ${XDG_DATA_HOME}/Steam/config/config.vdf.bak
-perl -0777 -i -pe 's/"CompatToolMapping"\n\s+{/"CompatToolMapping"\n\t\t\t\t{\n\t\t\t\t\t"39140"\n\t\t\t\t\t{\n\t\t\t\t\t\t"name"\t\t"proton_7"\n\t\t\t\t\t\t"config"\t\t""\n\t\t\t\t\t\t"priority"\t\t"250"\n\t\t\t\t\t}/gs' \
+perl -0777 -i -pe 's/"CompatToolMapping"\n\s+{/"CompatToolMapping"\n\t\t\t\t{\n\t\t\t\t\t"39140"\n\t\t\t\t\t{\n\t\t\t\t\t\t"name"\t\t"proton_experimental"\n\t\t\t\t\t\t"config"\t\t""\n\t\t\t\t\t\t"priority"\t\t"250"\n\t\t\t\t\t}/gs' \
 ${XDG_DATA_HOME}/Steam/config/config.vdf
 while pgrep "steam" > /dev/null; do sleep 1; done
-rm -rf "${WINEPATH%/pfx}"/*
+[ "${WINEPATH}" = */compatdata/39140/pfx ] && rm -rf "${WINEPATH%/pfx}"/*
 echo "Sign into the Steam account that owns FF7 if prompted."
 nohup steam steam://rungameid/39140 &> /dev/null &
 echo "Waiting for Steam..."
@@ -106,19 +137,19 @@ mkdir -p "${WINEPATH}/drive_c/ProgramData" # fix vcredist install - infirit
 STEAM_COMPAT_APP_ID=39140 STEAM_COMPAT_DATA_PATH="${WINEPATH%/pfx}" \
 STEAM_COMPAT_CLIENT_INSTALL_PATH=$(readlink -f "$HOME/.steam/root") \
 "$RUNTIME" -- "$PROTON" waitforexitandrun \
-$SEVENTH_INSTALLER /SILENT /DIR="Z:$INSTALL_PATH" &>> "7thDeck.log"
+"$SEVENTH_INSTALLER" /VERYSILENT /DIR="Z:$INSTALL_PATH" /LOG="7thHeaven.log" &>> "7thDeck.log"
+echo
 
 # Tweaks to 7th Heaven and FF7 directories
 echo "Applying patches..."
-mkdir "$INSTALL_PATH/7thWorkshop"
+mkdir -p "$INSTALL_PATH/7thWorkshop/profiles"
 cp -f "$INSTALL_PATH/Resources/FF7_1.02_Eng_Patch/ff7.exe" "$FF7_DIR/ff7.exe"
 cp -f deps/dxvk.conf "$INSTALL_PATH/"
 cp -f "deps/7th Heaven.sh" "$INSTALL_PATH/"
+cp -f "deps/functions.sh" "$INSTALL_PATH/"
 cp -f deps/settings.xml "$INSTALL_PATH/7thWorkshop/"
-sed -i "s|@RUNTIME_PATH@|$RUNTIME|" "$INSTALL_PATH/7th Heaven.sh"
-sed -i "s|@PROTON_PATH@|$PROTON|" "$INSTALL_PATH/7th Heaven.sh"
-sed -i "s|@MOUNTS@|$STEAM_COMPAT_MOUNTS|" "$INSTALL_PATH/7th Heaven.sh"
-sed -i "s|@WINEPATH@|${WINEPATH%/pfx}|" "$INSTALL_PATH/7th Heaven.sh"
+[ ! -f "$INSTALL_PATH/7thWorkshop/profiles/Default.xml" ] && cp "deps/Default.xml" "$INSTALL_PATH/7thWorkshop/profiles/" &>> "7thDeck.log"
+sed -i "s|@STEAMOS@|$IS_STEAMOS|" "$INSTALL_PATH/7th Heaven.sh"
 sed -i "s|<LibraryLocation>REPLACE_ME</LibraryLocation>|<LibraryLocation>Z:$INSTALL_PATH/mods</LibraryLocation>|" "$INSTALL_PATH/7thWorkshop/settings.xml"
 sed -i "s|<FF7Exe>REPLACE_ME</FF7Exe>|<FF7Exe>Z:$FF7_DIR/ff7.exe</FF7Exe>|" "$INSTALL_PATH/7thWorkshop/settings.xml"
 # Tweaks to proton prefix
@@ -130,7 +161,7 @@ echo
 # SteamOS only
 if [ $IS_STEAMOS = true ]; then
   # Steam Deck Auto-Config (mod)
-  mkdir "$INSTALL_PATH/mods"
+  mkdir -p "$INSTALL_PATH/mods"
   cp -rf deps/SteamDeckSettings "$INSTALL_PATH/mods/"
 
   # This allows moving and clicking the mouse by using the right track-pad without holding down the STEAM button
@@ -176,7 +207,7 @@ Terminal=false
 Type=Application
 StartupNotify=false" > "${XDG_DESKTOP_DIR}/7th Heaven.desktop"
 chmod +x "${XDG_DESKTOP_DIR}/7th Heaven.desktop"
-update-desktop-database ~/.local/share/applications &>> "7thDeck.log"
+update-desktop-database ${XDG_DATA_HOME}/applications &>> "7thDeck.log"
 echo
 
 # Add launcher to Steam
