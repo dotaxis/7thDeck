1740c360314cd14e261e14cc22ded46df808fb80
Chase Taylor <11805686+dotaxis@users.noreply.github.com>
2024-04-19 12:06:26 -0600

    Suppress General Settings window on first launch

--- install.sh
+++ install.sh
@@ -146,12 +146,13 @@ echo
 
 # Tweaks to 7th Heaven and FF7 directories
 echo "Applying patches..."
-mkdir -p "$INSTALL_PATH/7thWorkshop"
+mkdir -p "$INSTALL_PATH/7thWorkshop/profiles"
 cp -f "$INSTALL_PATH/Resources/FF7_1.02_Eng_Patch/ff7.exe" "$FF7_DIR/ff7.exe"
 cp -f deps/dxvk.conf "$INSTALL_PATH/"
 cp -f "deps/7th Heaven.sh" "$INSTALL_PATH/"
 cp -f "deps/functions.sh" "$INSTALL_PATH/"
 cp -f deps/settings.xml "$INSTALL_PATH/7thWorkshop/"
+[ ! -f "$INSTALL_PATH/7thWorkshop/profiles/Default.xml" ] && cp "deps/Default.xml" "$INSTALL_PATH/7thWorkshop/profiles/" &>> "7thDeck.log"
 sed -i "s|@STEAMOS@|$IS_STEAMOS|" "$INSTALL_PATH/7th Heaven.sh"
 sed -i "s|<LibraryLocation>REPLACE_ME</LibraryLocation>|<LibraryLocation>Z:$INSTALL_PATH/mods</LibraryLocation>|" "$INSTALL_PATH/7thWorkshop/settings.xml"
 sed -i "s|<FF7Exe>REPLACE_ME</FF7Exe>|<FF7Exe>Z:$FF7_DIR/ff7.exe</FF7Exe>|" "$INSTALL_PATH/7thWorkshop/settings.xml"
