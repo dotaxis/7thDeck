dffc0590227fa28905918b42808e3fd28a9664f8
Chase Taylor <11805686+dotaxis@users.noreply.github.com>
2024-05-19 01:22:56 -0600

    Select installation directory for 7th Heaven

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod steamhelper;
 
+use std::path::PathBuf;
+
 use steamhelper::proton;
 use steamhelper::game;
 use dialog::DialogBox;
@@ -7,7 +9,8 @@ use dialog::DialogBox;
 fn main() {
     let install_path = get_install_path();
     //install_7th(&install_path);
-    dialog::Message::new(format!("{:?}", game::get_game(39140).unwrap()))
+    dialog::Message::new(format!("Installing 7th Heaven to {:#?}", install_path.to_string_lossy()))
+        .title("Path confirmed.")
         .show()
         .expect("Failed to display dialog box.");
 }
@@ -31,6 +34,36 @@ fn install_7th(install_path: &str) {
     }
 }
 
-fn get_install_path() -> String {
-    "todo".to_string()
+fn get_install_path() -> PathBuf {
+    println!("Select an installation path for 7th Heaven.");
+    loop {
+        dialog::Message::new("Select an installation path for 7th Heaven.")
+            .title("Select Destination")
+            .show()
+            .expect("Failed to display dialog box.");
+
+        let install_path = match dialog::FileSelection::new("Select install path")
+            .title("Folder Selection")
+            .mode(dialog::FileSelectionMode::Save)
+            .show()
+            .expect("Failed to display file selection dialog box.") {
+                Some(path) => path,
+                None => {
+                    println!("No path selected. Retrying.");
+                    continue
+                }
+            };
+
+        let confirmed = dialog::Question::new(format!("7th Heaven will be installed to:\n{:#?}\nConfirm?", install_path))
+            .title("Confirm Install Location")
+            .show()
+            .expect("Failed to display dialog box.");
+
+        if confirmed != dialog::Choice::Yes {
+            println!("User did not confirm installation path. Retrying.");
+            continue;
+        }
+
+        return PathBuf::from(install_path);
+    }
 }
