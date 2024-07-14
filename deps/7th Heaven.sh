cd83575798fe7289a8056c6542e38b194673aa81
Chase Taylor <11805686+dotaxis@users.noreply.github.com>
2024-07-13 18:25:14 -0600

    Add/remove dxvk.conf based on gamescope session presence

--- deps/7th Heaven.sh
+++ deps/7th Heaven.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 . functions.sh
 
+IS_GAMESCOPE=$(pgrep gamescope > /dev/null && echo true || echo false)
+if [ $IS_GAMESCOPE = true ]; then
+  echo "d3d9.shaderModel = 1" > dxvk.conf
+else
+  [ -f "dxvk.conf" ] && rm dxvk.conf
+fi
+
 IS_STEAMOS=@STEAMOS@
 if [ $IS_STEAMOS = true ] ; then
   export STEAM_COMPAT_DATA_PATH=${HOME}/.steam/steam/steamapps/compatdata/39140
