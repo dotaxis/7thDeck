dd654043ba58b4d618b2b29da61a274127885d81
Chase Taylor <11805686+dotaxis@users.noreply.github.com>
2025-02-05 00:11:57 -0700

    Use Makefile instead of build script

---
+++ Makefile
@@ -0,0 +1,2 @@
+launcher:
+	cargo build --bin launcher
--- build.rs
+++
@@ -1,9 +0,0 @@
-use std::process::Command;
-
-fn main() {
-    println!("Building launcher!");
-    Command::new("cargo")
-        .args(["build", "--bin", "launcher"])
-        .status()
-        .expect("Failed to build launcher");
-}
